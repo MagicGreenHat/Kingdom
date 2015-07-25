2ed59efa65e3de65e854dc938da4b69936d46aa3
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-07-26 01:08:25 +0300

    Обновление websocket роутера

--- websocket/router.js
+++ websocket/router.js
@@ -1,27 +1,13 @@
-// Verbose output
-WAMPRT_TRACE = true;
+WAMPRT_TRACE = true; // Verbose output
 WEBSOCKET_PORT = 7777;
 
 var Router = require('wamp.rt');
-var program = require('commander');
 
-program.option('-p, --port <port>', 'Server IP port', parseInt, WEBSOCKET_PORT);
+// WebSocket router
+var app = new Router({port: WEBSOCKET_PORT});
 
-function onPublish(topicUri, args) {
-    console.log('[' + topicUri + ']: ', args);
-}
-
-// WebSocket server
-var app = new Router(
-    {
-        port: program.port,
-        handleProtocols: function (protocols, cb) {
-            console.log(protocols);
-            cb(true, protocols[0]);
-        }
-    }
-);
-
-app.on('Publish', onPublish);
+app.on('Publish', function (topicUri, args) {
+    console.log('[' + topicUri + ']:', args[0]);
+});
 
 console.log('WebSocket router is running ...');
