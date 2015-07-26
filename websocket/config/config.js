50f5be5b0aca29cb144811da66815bc2d2fe86fa
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-07-26 13:27:45 +0300

    Файл конфига для node.js серверов

---
+++ websocket/config/config.js
@@ -0,0 +1,6 @@
+// Unused
+Config = {
+    WEBSOCKET_SERVER_URL: 'ws://localhost:7777/',
+    WEBSOCKET_REALM: 'kingdom',
+    SECONDS_IN_TICK: 60
+};
--- websocket/test.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-
-# Count from 1 to 10 with a sleep
-for ($count = 1; $count <= 10; $count++) {
-	echo $count . "\n";
-	usleep(500000);
-}
