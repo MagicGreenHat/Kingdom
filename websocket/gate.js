38d0704fd4e6eab6d9e18bb74d273b13d2b2ee9d
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-07-26 13:24:06 +0300

    Регистрация удаленной команды в gate.js сервере

--- websocket/gate.js
+++ websocket/gate.js
@@ -3,6 +3,8 @@
  */
 
 COMMAND_CHANNEL_NAME = 'command';
+SYSTEM_CHANNEL_NAME = 'system';
+GATE_CHANNEL_NAME = 'system.register.hash';
 SYMFONY_CONSOLE_PATH = '../app/console';
 
 var autobahn = require('autobahn');
@@ -15,15 +17,20 @@ var connection = new autobahn.Connection({
 
 connection.onopen = function (session) {
 
-    session.subscribe(COMMAND_CHANNEL_NAME, function onevent(args) {
-        var cmd = SYMFONY_CONSOLE_PATH + ' --command ' + args[0];
+    //session.subscribe(COMMAND_CHANNEL_NAME, function onevent(args) {
+    //    var cmd = SYMFONY_CONSOLE_PATH + ' --command ' + args[0];
+    //
+    //    exec(cmd, function (error, stdout, stderr) {
+    //        session.publish(COMMAND_CHANNEL_NAME, [stdout]);
+    //    });
+    //});
 
-        exec(cmd, function (error, stdout, stderr) {
-            session.publish(COMMAND_CHANNEL_NAME, [stdout]);
-        });
+    session.publish(SYSTEM_CHANNEL_NAME, ['Gate service is running ...']);
+
+    session.register(GATE_CHANNEL_NAME, function (args) {
+        return args[0];
     });
 
-    session.publish(COMMAND_CHANNEL_NAME, ['Gate service']);
 };
 
 connection.open();
