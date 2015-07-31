aadc94471a81dc6b33e4742ce44100eb11124740
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-07-31 19:44:02 +0300

    Настройка snc redis bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Rottenwood\KingdomBundle\RottenwoodKingdomBundle(),
             new Rottenwood\UserBundle\RottenwoodUserBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
