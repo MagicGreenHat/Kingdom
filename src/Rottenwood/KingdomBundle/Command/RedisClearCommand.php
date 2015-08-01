b2af65faa7fa80b262c7678fe409e77597d4236a
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-01 22:00:20 +0300

    Симфони-команда удаления данных из redis

---
+++ src/Rottenwood/KingdomBundle/Command/RedisClearCommand.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Rottenwood\KingdomBundle\Command;
+
+use Rottenwood\KingdomBundle\Redis\RedisClientInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RedisClearCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this->setName('kingdom:redis:clear');
+        $this->setDescription('Очистка данных в redis');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        /** @var RedisClientInterface $redis */
+        $redis = $this->getContainer()->get('snc_redis.default');
+        $redis->del(RedisClientInterface::CHARACTERS_HASH_NAME);
+
+        $output->writeln('Данные удалены из redis');
+    }
+}
