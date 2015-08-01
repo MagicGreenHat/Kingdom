3046eec7b908fb01d8f20bab1ac2fdb69a168b10
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-01 21:47:27 +0300

    Симфони-команда для обработки пользовательских запросов

---
+++ src/Rottenwood/KingdomBundle/Command/ExecuteCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Rottenwood\KingdomBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ExecuteCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this->setName('kingdom:execute');
+        $this->setDescription('Входная точка игры');
+        $this->addArgument('userId',
+            InputArgument::REQUIRED,
+            'id игрока'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $userId = $input->getArgument('userId');
+
+        $result = [
+            'userId' => $userId,
+        ];
+
+        $output->writeln(json_encode($result));
+    }
+}
