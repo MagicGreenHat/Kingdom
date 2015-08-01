ed69d3d93d82b776099de22df2b1a98a394e282c
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-01 23:02:38 +0300

    В симфони-команду kingdom:execute добавлен второй аргумент - внешняя команда

--- src/Rottenwood/KingdomBundle/Command/ExecuteCommand.php
+++ src/Rottenwood/KingdomBundle/Command/ExecuteCommand.php
@@ -12,17 +12,25 @@ class ExecuteCommand extends ContainerAwareCommand {
     protected function configure() {
         $this->setName('kingdom:execute');
         $this->setDescription('Входная точка игры');
+
         $this->addArgument('userId',
             InputArgument::REQUIRED,
             'id игрока'
+        );
+
+        $this->addArgument('externalCommand',
+            InputArgument::REQUIRED,
+            'команда для выполнения'
         );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
         $userId = $input->getArgument('userId');
+        $command = $input->getArgument('externalCommand');
 
         $result = [
             'userId' => $userId,
+            'command' => $command,
         ];
 
         $output->writeln(json_encode($result));
