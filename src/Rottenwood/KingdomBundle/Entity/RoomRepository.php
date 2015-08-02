257a4a37cfc6581228ceef3347ac9be6c34e34bd
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-03 02:06:13 +0300

    В RoomRepository добавлен поиск по координатам

--- src/Rottenwood/KingdomBundle/Entity/RoomRepository.php
+++ src/Rottenwood/KingdomBundle/Entity/RoomRepository.php
@@ -6,4 +6,13 @@ use Doctrine\ORM\EntityRepository;
 
 class RoomRepository extends EntityRepository {
 
+    /**
+     * Поиск по координатам
+     * @param int $x
+     * @param int $y
+     * @return Room
+     */
+    public function findOneByXandY($x, $y) {
+        return $this->findOneBy(['x' => $x, 'y' => $y]);
+    }
 }
