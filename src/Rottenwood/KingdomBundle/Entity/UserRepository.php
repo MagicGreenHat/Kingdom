a6c34ebea41e00c1a62f8470ec14b1d4178da77c
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-05 21:46:34 +0300

    В UserRepository добавлен метод findById

--- src/Rottenwood/KingdomBundle/Entity/UserRepository.php
+++ src/Rottenwood/KingdomBundle/Entity/UserRepository.php
@@ -6,7 +6,25 @@ use Rottenwood\KingdomBundle\Entity\Infrastructure\AbstractRepository;
 
 class UserRepository extends AbstractRepository {
 
+    /**
+     * @param int $userId
+     * @return User
+     */
     public function findById($userId) {
         return $this->find($userId);
+    }
+
+    /**
+     * @param Room  $room
+     * @param int[] $onlinePlayersIds
+     * @return User[]
+     */
+    public function findOnlineByRoom($room, array $onlinePlayersIds) {
+        $builder = $this->createQueryBuilder('u');
+        $builder->where('u.room = :room');
+        $builder->andWhere($builder->expr()->in('u.id', $onlinePlayersIds));
+        $builder->setParameters(['room' => $room]);
+
+        return $builder->getQuery()->getResult();
     }
 }
