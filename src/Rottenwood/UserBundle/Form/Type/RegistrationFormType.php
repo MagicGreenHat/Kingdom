6142af906d7e729f3e1b65205bff1c5d30116a48
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-07 14:16:27 +0300

    Наследуемый из FOSUserBundle RegistrationFormType

---
+++ src/Rottenwood/UserBundle/Form/Type/RegistrationFormType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace FOS\UserBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends RegistrationFormType {
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add(
+            'email',
+            'email',
+            [
+                'label'              => 'form.email',
+                'translation_domain' => 'FOSUserBundle',
+            ]
+        );
+
+        $builder->add(
+            'username',
+            null,
+            [
+                'label'              => 'form.username',
+                'translation_domain' => 'FOSUserBundle',
+            ]
+        );
+
+        $builder->add(
+            'plainPassword',
+            'repeated',
+            [
+                'type'            => 'password',
+                'options'         => ['translation_domain' => 'FOSUserBundle'],
+                'first_options'   => ['label' => 'form.password'],
+                'second_options'  => ['label' => 'form.password_confirmation'],
+                'invalid_message' => 'fos_user.password.mismatch',
+            ]
+        );
+    }
+}
