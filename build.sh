2525006ef3d1975abf6dc86f5b129ed37e91b4ce
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-11 13:51:24 +0300

    В build.sh добавлено создание БД и обновление ее структуры

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 
 command -v docker >/dev/null 2>&1 || { echo "Докер не установлен. Установка: http://docs.docker.com/linux/started"; exit 1; }
 
+echo "Создание БД, при ее отсутствии ..."
+app/console doctrine:database:create > /dev/null 2>&1
+echo "Обновление структуры БД ..."
+app/console doctrine:schema:update --force
+
 docker stop kingdom > /dev/null 2>&1
 docker rm kingdom > /dev/null 2>&1
 docker rmi rottenwood/kingdom > /dev/null 2>&1
