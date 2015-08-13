71d040240cf779f6f5eeeafd2c0d965cda830f6c
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-13 19:07:34 +0300

    В скрипт управления окружением добавлены общие логи

--- app/docker/init.sh
+++ app/docker/init.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# Настройка для доступа www-data к внешним файлам
+echo "Настройка для доступа www-data к внешним файлам ..."
 usermod -u 1000 www-data
 
-# Копирование конфигов для nginx
+echo "Копирование конфигов для nginx ..."
 cp -r /kingdom/app/docker/nginx /etc
 ln -s /etc/nginx/sites-available/kingdom.conf /etc/nginx/sites-enabled/
 
-# Обновление библиотек композера
+echo "Обновление библиотек композера ..."
 [ -d /kingdom/vendor ] || mkdir /kingdom/vendor
 chown www-data:www-data /kingdom/vendor
 sudo -u www-data ~/composer.phar install -n -d /kingdom/
 
-# Создание БД, при ее отсутствии
+echo "Создание БД, при ее отсутствии ..."
 /kingdom/app/console doctrine:database:create > /dev/null 2>&1
 
-# Обновление структуры БД
+echo "Обновление структуры БД ..."
 /kingdom/app/console doctrine:schema:update --force
 
-# Загрузка игровых данных в БД
+echo "Загрузка игровых данных в БД ..."
 /kingdom/app/console kingdom:map:create
 /kingdom/app/console kingdom:items:create
 
-# Инициализация серверов
+echo "Инициализация серверов ..."
 /etc/init.d/php5-fpm start
 /etc/init.d/redis-server start
 /etc/init.d/nginx start
 
-# Запуск node.js приложений
+echo "Запуск node.js приложений ..."
 cd /kingdom/websocket
 (node router.js &) && node gate.js
