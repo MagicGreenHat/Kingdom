a170ae8ddc22326a78b6f7a2f20ba43243a2297e
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-21 18:33:56 +0300

    Симфони-команда для логирования событий node.js

---
+++ src/Rottenwood/KingdomBundle/Command/Console/NodeLogCommand.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Rottenwood\KingdomBundle\Command\Console;
+
+use Rottenwood\KingdomBundle\Exception\InvalidCommandParameter;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class NodeLogCommand extends ContainerAwareCommand {
+
+    protected function configure() {
+        $this->setName('kingdom:node:log');
+        $this->setDescription('Точка входа для логирования событий из node.js');
+
+        $this->addArgument('event',
+            InputArgument::REQUIRED,
+            'имя события'
+        );
+
+        $this->addArgument('userId',
+            InputArgument::REQUIRED,
+            'id игрока'
+        );
+
+        $this->addArgument('userName',
+            InputArgument::REQUIRED,
+            'имя игрока'
+        );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $logger = $this->getContainer()->get('kingdom.logger');
+
+        $event = $input->getArgument('event');
+        $userId = $input->getArgument('userId');
+        $userName = $input->getArgument('userName');
+
+        if ($event == 'userEnter') {
+            $eventPhrase = 'присоединился к игре';
+        } elseif ($event == 'userExit') {
+            $eventPhrase = 'вышел из игры';
+        } else {
+            throw new InvalidCommandParameter('Неизвестное событие: ' . $event);
+        }
+
+        $logger->info(sprintf('[%d]%s %s', $userId, $userName, $eventPhrase));
+    }
+}
