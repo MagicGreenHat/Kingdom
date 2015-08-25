9d5fd6cc2d9782c0f34bbb0448717687f6ab078b
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-26 00:59:46 +0300

    Контроллер открытия и отображения блока инвентаря и денег персонажа

---
+++ frontend/Controller/Inventory.js
@@ -0,0 +1,48 @@
+/**
+ * Открытие и отображение блока инвентаря персонажа
+ */
+$(function () {
+    var $room = $('#game-room');
+    var $inventory = $('#game-inventory');
+    var $userInfo = $('#game-user-info');
+    var $openButton = $('.open-inventory-button');
+
+    $openButton.click(function () {
+        $inventory.show();
+        $room.hide();
+        $userInfo.hide();
+    });
+
+    $inventory.find('.close-button').click(function () {
+        $room.show();
+        $userInfo.hide();
+        $inventory.hide();
+    });
+
+    $openButton.on('click', function () {
+        var html = '<div>Золото: ' + Kingdom.Money.getGold() + '</div>'
+            + '<div>Серебро: ' + Kingdom.Money.getSilver() + '</div>';
+
+        $('#game-inventory .money').html(html);
+    });
+
+    /**
+     * Отрисовка инвентаря
+     */
+    function renderInventory() {
+        var html = '';
+        var inventory = Kingdom.Inventory.getItems();
+
+        inventory.done(function () {
+            var items = inventory.items;
+
+            items.forEach(function (item) {
+                html += '<div class="item"><img src="/img/items/' + item.pic + '.png"></div>';
+            });
+
+            $('#game-inventory .items-list').html(html);
+        });
+    }
+
+    renderInventory();
+});
--- frontend/Controller/InventoryMoney.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.open-inventory-button').on('click', function () {
-        var html = '<div>Золото: ' + Kingdom.Money.getGold() + '</div>'
-            + '<div>Серебро: ' + Kingdom.Money.getSilver() + '</div>';
-
-        $('#game-inventory .money').html(html);
-    });
-});
