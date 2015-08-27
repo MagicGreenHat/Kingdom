653001ff734b9d3233b660c6e7cb4fd9c817baf1
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-27 18:33:32 +0300

    В проект добавлен gulp-add-src и удален rigger

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var gulp = require('gulp'),
     prefixer = require('gulp-autoprefixer'),
     uglify = require('gulp-uglify'),
     sourcemaps = require('gulp-sourcemaps'),
-    rigger = require('gulp-rigger'),
     cssmin = require('gulp-minify-css'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename');
@@ -15,10 +14,12 @@ var path = {
         css: 'web/css'
     },
     src: {
+        jsLib: [
+            'frontend/Library/**/*.js'
+        ],
         js: [
             'frontend/namespace.js', // Загрузка пространства имен вначале
             'web/js/src/**/*.js',
-            'frontend/Library/**/*.js',
             'frontend/Model/**/*.js',
             'frontend/Controller/**/*.js' // Загрузка контроллеров в конце
         ],
@@ -33,7 +34,6 @@ var path = {
 gulp.task('js:build', function () {
     gulp.src(path.src.js)
         .pipe(concat(path.destination.js))
-        .pipe(rigger())
         .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(sourcemaps.write())
