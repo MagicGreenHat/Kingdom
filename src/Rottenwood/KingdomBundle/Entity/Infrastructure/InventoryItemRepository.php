3e21aca5fe81c267f74d365cd635e9662a87e93e
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-08-29 02:18:20 +0300

    Метод в InventoryItemRepository для поиска предмета по игроку и слоту

--- src/Rottenwood/KingdomBundle/Entity/Infrastructure/InventoryItemRepository.php
+++ src/Rottenwood/KingdomBundle/Entity/Infrastructure/InventoryItemRepository.php
@@ -36,4 +36,25 @@ class InventoryItemRepository extends AbstractRepository {
 
         return $builder->getQuery()->getSingleResult();
     }
+
+    /**
+     * Поиск предмета по игроку и слоту
+     * @param User $user
+     * @param string $slotName
+     * @return InventoryItem
+     */
+    public function findOneByUserAndSlot($user, $slotName) {
+        $builder = $this->createQueryBuilder('inventory_item');
+        $builder->select('inventory_item');
+        $builder->where('inventory_item.user = :user');
+        $builder->andWhere('inventory_item.slot = :slotName');
+        $builder->setParameters(
+            [
+                'user'     => $user,
+                'slotName' => $slotName,
+            ]
+        );
+
+        return $builder->getQuery()->getSingleResult();
+    }
 }
