97a07ec2370e35a6f17d665ed9bf70f90f5ebe25
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-09-02 23:48:59 +0300

    Команда создания комнат из yaml-файла

--- src/Rottenwood/KingdomBundle/Command/Console/CreateMapCommand.php
+++ src/Rottenwood/KingdomBundle/Command/Console/CreateMapCommand.php
@@ -2,58 +2,84 @@
 
 namespace Rottenwood\KingdomBundle\Command\Console;
 
+use Rottenwood\KingdomBundle\Entity\Infrastructure\AbstractRepository;
+use Rottenwood\KingdomBundle\Entity\Infrastructure\RoomType;
 use Rottenwood\KingdomBundle\Entity\Room;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Yaml\Yaml;
 
 class CreateMapCommand extends ContainerAwareCommand {
 
+    /** {@inheritdoc} */
     protected function configure() {
         $this->setName('kingdom:create:map')->setDescription('Создание карты мира');
     }
 
+    /** {@inheritdoc} */
     protected function execute(InputInterface $input, OutputInterface $output) {
         $container = $this->getContainer();
-
         $repository = $container->get('kingdom.room_repository');
-
         $rooms = $repository->findAll();
 
         if (count($rooms)) {
             $output->writeln(sprintf('Уже создано %d комнат. Удалите их командой kingdom:purge:map', count($rooms)));
         } else {
-            $output->write('Создание типов комнат ... ');
-
-            $fileFinder = new Finder();
-
-            /** @var SplFileInfo[] $typeClasses */
-            $typeClasses = $fileFinder->files()->name('*.php')->in(__DIR__ . '/../../Entity/RoomTypes');
-
-            $typeNamespace = 'Rottenwood\\KingdomBundle\\Entity\\RoomTypes\\';
-
-            $roomTypes = [];
-            foreach ($typeClasses as $typeFile) {
-                $typeClass = $typeNamespace . $typeFile->getBasename('.php');
-                $roomType = new $typeClass();
-                $roomTypes[] = $roomType;
-                $repository->persist($roomType);
-            }
-
-            $output->write('Создание новых комнат ... ');
-
-            for ($y = -3; $y <= 3; $y++) {
-                for ($x = -3; $x <= 3; $x++) {
-                    $room = new Room($x, $y, $roomTypes[array_rand($roomTypes)]);
-                    $repository->persist($room);
-                }
-            }
+            $roomTypes = $this->createRoomTypes($repository, $output);
+            $this->createRooms($repository, $roomTypes, $output);
 
             $repository->flush();
-
-            $output->writeln(sprintf('Создано %d новых комнат.', count($repository->findAll())));
         }
+    }
+
+    /**
+     * @param AbstractRepository $repository
+     * @param OutputInterface    $output
+     * @return RoomType[]
+     */
+    private function createRoomTypes(AbstractRepository $repository, OutputInterface $output) {
+        /** @var SplFileInfo[] $typeClasses */
+        $typeClasses = (new Finder())->files()->name('*.php')->in(__DIR__ . '/../../Entity/RoomTypes');
+        $typeNamespace = 'Rottenwood\\KingdomBundle\\Entity\\RoomTypes\\';
+
+        $output->write('Создание типов комнат ... ');
+
+        $roomTypes = [];
+        foreach ($typeClasses as $typeFile) {
+            $typeName = $typeFile->getBasename('.php');
+            $typeClass = $typeNamespace . $typeName;
+            $roomType = new $typeClass();
+            $roomTypes[strtolower($typeName)] = $roomType;
+            $repository->persist($roomType);
+        }
+
+        $output->writeln(sprintf('Создано %d новых типов комнат.', count($roomTypes)));
+
+        return $roomTypes;
+    }
+
+    /**
+     * @param AbstractRepository $repository
+     * @param RoomType[]         $roomTypes
+     * @param OutputInterface    $output
+     * @return Room[]
+     */
+    private function createRooms(AbstractRepository $repository, array $roomTypes, OutputInterface $output) {
+        $yamlParser = new Yaml();
+        $roomsData = $yamlParser->parse(__DIR__ . '/../../Resources/rooms/rooms.yml');
+
+        $output->write('Создание новых комнат ... ');
+
+        $newRooms = [];
+        foreach ($roomsData as $roomData) {
+            $room = $room = new Room($roomData['x'], $roomData['y'], $roomTypes[$roomData['type']]);
+            $newRooms[] = $room;
+            $repository->persist($room);
+        }
+
+        $output->writeln(sprintf('Создано %d новых комнат.', count($newRooms)));
     }
 }
