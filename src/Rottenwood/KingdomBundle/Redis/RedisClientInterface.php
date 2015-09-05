5ebdedff5a36904b087cf036af66f168cb658fcf
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-09-05 14:41:07 +0300

    В интерфейс редиса добавлена константа хэша комнат в которых находятся персонажи

--- src/Rottenwood/KingdomBundle/Redis/RedisClientInterface.php
+++ src/Rottenwood/KingdomBundle/Redis/RedisClientInterface.php
@@ -9,6 +9,7 @@ interface RedisClientInterface {
 
     const ID_USERNAME_HASH = 'kingdom:users:usernames';
     const ID_SESSION_HASH = 'kingdom:users:sessions';
+    const ID_ROOM_HASH = 'kingdom:users:rooms';
     const SESSION_ID_HASH = 'kingdom:sessions:users';
     const ONLINE_LIST = 'kingdom:users:online';
 
