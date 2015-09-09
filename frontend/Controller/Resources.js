19b621eab4eff2e129d87de81acf119a7d17bdc4
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-09-09 15:21:23 +0300

    Отображение сообщение игроку о том, что он рубит дерево

--- frontend/Controller/Resources.js
+++ frontend/Controller/Resources.js
@@ -1,5 +1,6 @@
 $(function () {
     $('#game-room').on('click', '.resource-obtain.wood.button', function () {
         Kingdom.Websocket.command('obtainWood');
+        Kingdom.Chat.addInfo('Ты рубишь дерево');
     });
 });
