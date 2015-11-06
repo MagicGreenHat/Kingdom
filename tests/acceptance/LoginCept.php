51e0c683be8589ba38ed89f1809c44db82a8f5bd
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-07 01:59:13 +0300

    Тест на авторизацию пользователя по логину и паролю. Дамп базы данных для тестов

--- tests/acceptance/LoginCept.php
+++ tests/acceptance/LoginCept.php
@@ -1,7 +1,12 @@
 <?php
 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('страница логина отображается корректно');
+$I->wantTo('страница логина отображается корректно, авторизация по паролю работает');
 $I->amOnPage('/');
 $I->see('Войти');
 $I->see('Регистрация');
+
+$I->fillField('#username', 'test');
+$I->fillField('#password', 'test');
+$I->click('Войти');
+$I->see('Здравствуй, Тест!');
