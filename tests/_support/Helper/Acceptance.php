943b590ba7b0bcf38758dbd9b5448e72a754fc08
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-08 00:53:28 +0300

    Метод для приемочных тестов для логина под выбранным пользователем

--- tests/_support/Helper/Acceptance.php
+++ tests/_support/Helper/Acceptance.php
@@ -1,9 +1,34 @@
 <?php
 namespace Helper;
-// here you can define custom actions
-// all public methods declared in helper class will be available in $I
 
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
+/**
+ * here you can define custom actions
+ * all public methods declared in helper class will be available in $I
+ * @package Helper
+ */
 class Acceptance extends \Codeception\Module
 {
 
+    public function amLoggedInAs($username)
+    {
+        $symfonyModule = $this->getModule('Symfony2');
+        $container = $symfonyModule->container;
+
+        $userRepository = $container->get('kingdom.user_repository');
+
+        $user = $userRepository->findOneBy(['username' => $username]);
+
+        $firewall = 'main';
+        $token = new UsernamePasswordToken($user, null, $firewall, $user->getRoles());
+
+        /** @var Session $session */
+        $session = $container->get('session');
+        $session->set('_security_' . $firewall, serialize($token));
+        $session->save();
+
+        $symfonyModule->setCookie($session->getName(), $session->getId());
+    }
 }
