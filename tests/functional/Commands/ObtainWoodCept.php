9c9aac7c910d0569f2086937b7cdcbe9213ccc2d
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-15 20:22:06 +0300

    Функциональный тест на сбор древесины изменен: добавлено ожидание задержки при добыче

--- tests/functional/Commands/ObtainWoodCept.php
+++ tests/functional/Commands/ObtainWoodCept.php
@@ -17,17 +17,5 @@ PHPUnit_Framework_Assert::assertEquals(1, $result['data']['obtained']);
 PHPUnit_Framework_Assert::assertEquals(9, $result['data']['resources']['wood']);
 
 $result = $I->runCommand('obtainWood');
-PHPUnit_Framework_Assert::assertEquals(1, $result['data']['obtained']);
-PHPUnit_Framework_Assert::assertEquals(8, $result['data']['resources']['wood']);
-
-$result = $I->runCommand('obtainWood');
-PHPUnit_Framework_Assert::assertEquals(1, $result['data']['obtained']);
-PHPUnit_Framework_Assert::assertEquals(7, $result['data']['resources']['wood']);
-
-
-foreach (range(1, 10) as $i) {
-    $result = $I->runCommand('obtainWood');
-}
-
-PHPUnit_Framework_Assert::assertEquals('obtainWood', $result['commandName']);
-PHPUnit_Framework_Assert::assertFalse(isset($result['data']));
+PHPUnit_Framework_Assert::assertEquals(0, $result['data']['obtained']);
+PHPUnit_Framework_Assert::assertTrue($result['data']['delay']);
