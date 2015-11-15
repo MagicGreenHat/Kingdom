b0559108fd2d1715d9738973391256ab1c704d77
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-15 22:57:51 +0300

    Кастомный логгер для регистрации юзеров

---
+++ src/Rottenwood/UserBundle/Loggers/RegistrationLogger.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author: Rottenwood
+ * @date  : 15.11.15 22:54
+ */
+
+namespace Rottenwood\UserBundle\Loggers;
+
+use Monolog\Logger;
+
+class RegistrationLogger extends Logger
+{
+
+}
