880a3810162b1de2750c33c2581ceb071eb7b8dd
Petr Karmashev <petr@quank.org>
2015-11-20 00:47:43 +0600

    Create CreateRobotCommand.php

---
+++ src/Rottenwood/KingdomBundle/Command/Console/CreateRobotCommand.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rottenwood\KingdomBundle\Command\Console;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CreateRobotCommand extends ContainerAwareCommand {
+
+}
