561385d8f3f4eae00dbc8bf470eba34df1f64190
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-26 01:04:42 +0300

    Настройка тестовой базы для всех видов тестов

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,2 +1,8 @@
 <?php
-// This is global bootstrap for autoloading
+
+codecept_debug(`/kingdom/app/console doctrine:database:drop --force -e test`);
+codecept_debug(`/kingdom/app/console doctrine:database:create -e test`);
+codecept_debug(`/kingdom/app/console doctrine:schema:update --force -e test`);
+codecept_debug(`/kingdom/app/console kingdom:create:map -e test`);
+codecept_debug(`/kingdom/app/console kingdom:create:user test test test@test.ru -e test`);
+codecept_debug(`/kingdom/app/console kingdom:create:items -e test`);
--- tests/functional/_bootstrap.php
+++ tests/functional/_bootstrap.php
@@ -1,10 +1,2 @@
 <?php
 
-// Настройка базы перед запуском функциональных тестов
-
-codecept_debug(`/kingdom/app/console doctrine:database:drop --force -e test`);
-codecept_debug(`/kingdom/app/console doctrine:database:create -e test`);
-codecept_debug(`/kingdom/app/console doctrine:schema:update --force -e test`);
-codecept_debug(`/kingdom/app/console kingdom:create:map -e test`);
-codecept_debug(`/kingdom/app/console kingdom:create:user test test test@test.ru -e test`);
-codecept_debug(`/kingdom/app/console kingdom:create:items -e test`);
