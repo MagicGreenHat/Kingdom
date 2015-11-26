880e2018315767a7a44d0717f4b382bd4891a797
Petr Karmashev (Rottenwood) <smonkl@bk.ru>
2015-11-26 23:24:35 +0300

    Приемочный тест страницы регистрации

---
+++ tests/acceptance/RegistrationCept.php
@@ -0,0 +1,22 @@
+<?php
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('Register and see starter items in inventory');
+
+$I->amOnPage('/');
+
+$I->see('Войти');
+$I->see('Регистрация');
+
+$I->click('Регистрация');
+
+$I->see('Регистрация в Государстве');
+
+$I->fillField('#fos_user_registration_form_username', 'tester');
+$I->fillField('#fos_user_registration_form_email', 'tester@tester.ru');
+$I->fillField('#fos_user_registration_form_plainPassword_first', 'tester');
+$I->fillField('#fos_user_registration_form_plainPassword_second', 'tester');
+
+$I->click('Регистрация');
+
+$I->see('Здравствуй, Тестер!');
