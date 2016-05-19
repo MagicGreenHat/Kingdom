488ab930ec6b218e7c794547248ae147659b4c8c
Petr Karmashev <petr@quank.org>
2016-05-19 21:36:52 +0300

    Update autoload.php

--- app/autoload.php
+++ app/autoload.php
@@ -3,10 +3,8 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
-/**
- * @var ClassLoader $loader
- */
-$loader = require __DIR__.'/../vendor/autoload.php';
+/** @var ClassLoader $loader */
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
